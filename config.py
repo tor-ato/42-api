1fdf4b75734983f4a72cc167873a4512041da1ec
tor-ato <toratokitahara@gmail.com>
2025-05-16 17:14:27 +0900

    init

---
+++ config.py
@@ -0,0 +1,2 @@
+API_URL = "https://api.intra.42.fr"
+CAMPUS_TOKYO = 26
---
+++ get_token.py
@@ -0,0 +1,20 @@
+from config import API_URL, CAMPUS_TOKYO
+from auth_config import UID, SECRET
+import requests
+
+def get_token():
+    token_url = f"{API_URL}/oauth/token"
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': UID,
+        'client_secret': SECRET
+    }
+    response = requests.post(token_url, data=payload)
+    if response.status_code == 200:
+        token_data = response.json()
+        access_token = token_data.get('access_token')
+        return access_token
+    else:
+        print(f"get token error : {response.status_code}")
+        print(response.text)
+        return None
